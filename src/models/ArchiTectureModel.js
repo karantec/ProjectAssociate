062026d6be38a88a1e6359736b44ffc7847f0dc0
KARAN RANA <103360966+karantec@users.noreply.github.com>
2025-01-12 11:25:17 +0530

    Update ArchiTectureModel.js

--- src/models/ArchiTectureModel.js
+++ src/models/ArchiTectureModel.js
@@ -2,31 +2,50 @@
 const mongoose = require('mongoose');
 
 const modalDataSchema = new mongoose.Schema({
-  title: { type: String },
-  clientName: { type: String },
-  projectType: { type: String },
+  title: { type: String, required: true },
+  clientName: { type: String, required: true },
   siteAddress: { type: String },
   gstNo: { type: String },
-  mahareraNo: { type: String },
   projectHead: { type: String },
   rccDesignerName: { type: String },
-  Pan: { type: String },
+  PAN: { type: String },
   Aadhar: { type: String },
   Pin: { type: String },
   email: { type: String },
-  Presentation_Drawing: [String],
-  File_Model_3D: [String],
-  Drawings: [String],
-  Working_Drawings: [String],
-  All_Floor: [String],
-  All_Section: [String],
-  All_Elevation: [String],
-  Site_Photo: [String],
+  documentSections: {
+    Area_Calculations: [{ type: String }],
+    Presentation_Drawings: [{ type: String }],
+    Submission_Drawings: [{ type: String }],
+    Center_Line: [{ type: String }],
+    Floor_Plans: [{ type: String }],
+    Sections: [{ type: String }],
+    Elevations: [{ type: String }],
+    Compound_Wall_Details: [{ type: String }],
+    Toilet_Layouts: [{ type: String }],
+    Electric_Layouts: [{ type: String }],
+    Tile_Layouts: [{ type: String }],
+    Grill_Details: [{ type: String }],
+    Railing_Details: [{ type: String }],
+    Column_footing_Drawings: [{ type: String }],
+    Plinth_Beam_Drawings: [{ type: String }],
+    StairCase_Details: [{ type: String }],
+    Slab_Drawings: [{ type: String }],
+    Property_Card: [{ type: String }],
+    Property_Map: [{ type: String }],
+    Sanction_Drawings: [{ type: String }],
+    Revise_Sanction_Drawings: [{ type: String }],
+    Completion_Drawings: [{ type: String }],
+    Completion_Letter: [{ type: String }],
+    Estimate: [{ type: String }],
+    Bills_Documents: [{ type: String }],
+    Site_Photos: [{ type: String }],
+    Other_Documents: [{ type: String }],
+  },
 });
 
 modalDataSchema.pre('save', async function (next) {
   // Pre-save hook logic can be added here
-  next();
+  next(); 
 });
 
 module.exports = mongoose.model("ModalData", modalDataSchema);
